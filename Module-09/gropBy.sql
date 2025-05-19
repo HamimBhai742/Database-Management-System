79055e405c8792c9a6ed281ac771d5a57c03cfa8
HamimBhai742 <omagootruelove147@gmail.com>
2025-05-19 09:03:50 +0600

    Grop By is complete

--- Module-09/gropBy.sql
+++ Module-09/gropBy.sql
@@ -1,1 +1,10 @@
 -- Active: 1747447723202@@127.0.0.1@5432@my_db
+
+SELECT * FROM students;
+
+SELECT age FROM students GROUP BY age HAVING age >22;
+
+SELECT age,count(*) FROM students GROUP BY age ;
+
+--last lgin
+SELECT extract(year from last_login) as last_login_year, count(*) FROM students GROUP BY last_login_year;
